3adb1c5702996517ed3a0b88ec0812d61d7fba2f
ronggurmahendra <ronggur.mwp@gmail.com>
2023-09-20 09:37:43 +0700

    add testing to ench and fix bugs

--- ench.m
+++ ench.m
@@ -1,7 +1,9 @@
 function main()
-    
     testContrastStreching()
-
+    testImageBrightening()
+    testImageNegative()
+    testImageLog()
+    testImagePow()
 end
 
 function testContrastStreching()
@@ -17,7 +19,68 @@ function testContrastStreching()
     colour_contraststreching(image_color);
     figure;
     greyscale_contraststreching(image_grey);
+end
 
+function testImageBrightening()
+    % Image Brightening
+    % Load image
+    image_color = imread('sample.png');  
+    image_grey = rgb2gray(image_color);
+    
+    imshow(image_color);
+    figure;
+    imshow(image_grey);
+    figure;
+    colour_imagebrightening(image_color,2,10);
+    figure;
+    greyscale_imagebrightening(image_grey,2,10);
+end
+
+function testImageNegative()
+    % Image Negative
+    % Load image
+    image_color = imread('sample.png');  
+    image_grey = rgb2gray(image_color);
+    
+    imshow(image_color);
+    figure;
+    imshow(image_grey);
+    figure;
+    colour_imagenegative(image_color);
+    figure;
+    greyscale_imagenegative(image_grey);
+end
+
+
+function testImageLog()
+    % Image Logarithm
+    % Load image
+    image_color = imread('sample.png');  
+    image_grey = rgb2gray(image_color);
+    
+    imshow(image_color);
+    figure;
+    imshow(image_grey);
+    figure;
+    colour_imagelog(image_color,1.5,1.5);
+    figure;
+    greyscale_imagelog(image_grey,1.5,1.5);
+end
+
+
+function testImagePow()
+    % Image power
+    % Load image
+    image_color = imread('sample.png');  
+    image_grey = rgb2gray(image_color);
+    
+    imshow(image_color);
+    figure;
+    imshow(image_grey);
+    figure;
+    colour_imagePow(image_color,2,2);
+    figure;
+    greyscale_imagePow(image_grey,2,2);
 end
 
 % asumsi 8 bit
@@ -60,7 +123,7 @@ function greyscale_imagenegative(image)
 
 end
     
-function colour_imagenegative(image, a, b)
+function colour_imagenegative(image)
     [rows, cols, channel] = size(image);
     
     for i = 1:rows
@@ -81,7 +144,7 @@ function greyscale_imagelog(image,c,r)
     for i = 1:rows
         for j = 1:cols
             % asumsi 8 bit
-            image(i,j) = max(min(c * log( image(i,j) + r ), 255),0); 
+            image(i,j) = max(min(c * log( double(image(i,j)) + r ), 255),0); 
         end
     end
     imshow(image);
@@ -95,27 +158,27 @@ function colour_imagelog(image, c,r)
         for j = 1:cols
             for k = 1:channel
                 % asumsi 8 bit
-                image(i,j,k) = max(min(c * log( image(i,j,k) + r ), 255),0); 
+                image(i,j,k) = max(min(c * log( double(image(i,j,k)) + r ), 255),0); 
             end
         end
     end
     imshow(image);
 end
 
-function greyscale_imagepow(image,c,y)
+function greyscale_imagePow(image,c,y)
     [rows, cols] = size(image);
     
     for i = 1:rows
         for j = 1:cols
             % asumsi 8 bit
-            image(i,j) = max(min(c * ( image(i,j) ^ r ), 255),0); 
+            image(i,j) = max(min(c * ( image(i,j) ^ y ), 255),0); 
         end
     end
     imshow(image);
 
 end
     
-function colour_imagepow(image, c,y)
+function colour_imagePow(image, c,y)
     [rows, cols, channel] = size(image);
     
     for i = 1:rows
