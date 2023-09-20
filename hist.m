a288a22312eb953b73083b5387b23e52912554de
ronggurmahendra <ronggur.mwp@gmail.com>
2023-09-20 19:00:55 +0700

    feat : histeq for colour

--- hist.m
+++ hist.m
@@ -18,12 +18,9 @@ function testHist()
     imshow(image_gray);
 
     % grayscale hist
-    figure;
     grayscale_hist(image_gray);
-    % grayscale hist
-    
-    % figure;
-    % colour_hist(image_color);
+    % colour hist
+    colour_hist(image_color);
 end
 
 function testHisteq()
@@ -98,14 +95,99 @@ function [R, G, B] = colour_hist(image)
 end
 
 function outputImage = colour_histEq(image)
+    % hist = zeros(1,256);
+    R = zeros(1, 256);
+    G = zeros(1, 256);
+    B = zeros(1, 256);  
+    
+    [rows cols, channel ] = size(image);    
+    no_of_pixels = rows*cols;
+    n = 0 : 255;
+    
+    % for i= 1:rows      
+    %     for j=1:cols
+    %         hist(Image(i,j)+1) = hist(Image(i,j)+1)+1;
+    %     end 
+    % end 
+    for i = 1:rows
+        for j = 1:cols
+            R(image(i,j,1) + 1)  = R(image(i,j,1) + 1) + 1;
+            G(image(i,j,2) + 1)  = G(image(i,j,2) + 1) + 1;
+            B(image(i,j,3) + 1)  = B(image(i,j,3) + 1) + 1;
+        end
+    end
 
+    % for i=1:256
+    %     hist(i)=hist(i)/no_of_pixels;
+    % end
+    for i=1:256
+        R(i)=R(i)/no_of_pixels;
+    end    
+    for i=1:256
+        G(i)=G(i)/no_of_pixels;
+    end    
+    for i=1:256
+        B(i)=B(i)/no_of_pixels;
+    end
+    
+    % temp=hist(1);
+    % for i=2:256
+    %     temp=temp+hist(i);
+    %     hist(i)=temp;
+    % end
 
-    outputImage = image
+    tempR=R(1);
+    for i=2:256
+        tempR=tempR+R(i);
+        R(i)=tempR;
+    end
+
+    tempG=G(1);
+    for i=2:256
+        tempG=tempG+G(i);
+        G(i)=tempG;
+    end
+
+    tempB=B(1);
+    for i=2:256
+        tempB=tempB+B(i);
+        B(i)=tempB;
+    end
+
+    % for i = 1:rows 
+    %     for j = 1:cols
+    %         outputImage(i,j) = hist(Image(i,j)+1);
+    %     end
+    % end
+    for i = 1:rows 
+        for j = 1:cols
+            R_out(i,j) = R(image(i,j,1)+1);
+        end
+    end
+    for i = 1:rows 
+        for j = 1:cols
+            G_out(i,j) = G(image(i,j,2)+1);
+        end
+    end
+    for i = 1:rows 
+        for j = 1:cols
+            B_out(i,j) = B(image(i,j,3)+1);
+        end
+    end
+    % outputImage = cat(3, uint8(R_out), uint8(G_out), uint8(B_out));
+    for i = 1:rows 
+        for j = 1:cols
+            outputImage(i,j,1) = R_out(i,j);
+            outputImage(i,j,2) = G_out(i,j);
+            outputImage(i,j,3) = B_out(i,j);
+        end
+    end
+    figure;
+    imshow(outputImage);
 end
 
 
 function outputImage = grayscale_histEq(Image)
-    
     hist = zeros(1,256);  
     
     [rows cols] = size(Image);    
